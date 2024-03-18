7ec6e172e22d80f6a51487f0b93053b7566f3eef
Nenavath Anil kumar <126111416+Anil1432kumar@users.noreply.github.com>
2024-03-18 07:08:55 +0530

    Add files via upload

--- files/script.js
+++ files/script.js
@@ -1,17 +1,18 @@
 var typed = new Typed(".text", {
     strings: ["Cyber security", "Web Development", "Ethical Hacking"],
-    typeSpeed: 100,
-    backSpeed: 100,
+    typeSpeed: 50,
+    backSpeed: 50,
     backDelay: 1000,
     loop: true
 });
 
-
 const toTop = document.querySelector(".top");
+
+
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
         toTop.classList.add("active");
     } else {
         toTop.classList.remove("active");
     }
-})
+});
